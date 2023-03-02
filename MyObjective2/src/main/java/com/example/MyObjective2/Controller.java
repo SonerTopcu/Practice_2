db728e7e95be5e109e4b70587d9eaeec3eb0ac12
Soni <sonertopcu23@gmail.com>
2023-03-02 16:01:28 +0300

    Add files via upload

--- MyObjective2/src/main/java/com/example/MyObjective2/Controller.java
+++ MyObjective2/src/main/java/com/example/MyObjective2/Controller.java
@@ -1,36 +1,45 @@
 package com.example.MyObjective2;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Controller {
     public String checkTCNumber(String input) {
-        int odd = 0;
-        int even = 0;
         String result = "Correct TC number";
         Pattern pattern = Pattern.compile("\\d{11}");
         Matcher matcher = pattern.matcher(input);
 
         if (matcher.matches()) {
             String[] strArr = input.split("");
-            int[] numbers = new int[strArr.length];
+            int[] number = new int[strArr.length];
+            int[] odd = new int[5];
+            int[] even = new int[5];
+            int oddCount = 0;
+            int evenCount = 0;
 
             for (int i = 0; i < strArr.length; i++) {
-                numbers[i] = Integer.parseInt(strArr[i]);
+                number[i] = Integer.parseInt(strArr[i]);
             }
 
-            if (numbers[0] != 0) {
-                for (int i = 0; i <= 8; i++){
-                    if (i % 2 == 0){
-                        odd += numbers[i];
+            if (number[0] != 0){
+                for(int i = 0; i <= 8; i++){
+                    if(i % 2 == 0){
+                        odd[oddCount] = number[i];
+                        oddCount++;
                     }
                     else {
-                        even += numbers[i];
+                        even[evenCount] = number[i];
+                        evenCount++;
                     }
                 }
 
-                if (numbers[9] == (odd * 7 - even) % 10){
-                    if ((odd + even + numbers[9]) % 10 != numbers[10]){
+                if (number[9] == (Arrays.stream(odd).sum() * 7 - Arrays.stream(even).sum()) % 10){
+                    if (number[10] != (Arrays.stream(number).sum() - number[10]) % 10){
                         result = "Wrong TC number, 11th digit is incorrect.";
                     }
                 }
@@ -38,13 +47,13 @@ public class Controller {
                     result = "Wrong TC number, 10th digit is incorrect.";
                 }
             }
-            else {
+            else{
                 result = "Wrong TC number, first digit cannot be 0.";
             }
         }
-        else {
-                result = "Wrong input, please enter 11 digits.";
-            }
-            return result;
+        else{
+            result = "Wrong input, please enter 11 digits.";
+        }
+        return result;
     }
 }
--- MyObjective2/src/main/java/com/example/MyObjective2/MyObjective2Application.java
+++ MyObjective2/src/main/java/com/example/MyObjective2/MyObjective2Application.java
@@ -1,24 +1,29 @@
 package com.example.MyObjective2;
 
+import org.apache.catalina.connector.InputBuffer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class MyObjective2Application {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		ConfigurableApplicationContext ctx = new SpringApplicationBuilder(MyObjective2Application.class)
 				.web(WebApplicationType.NONE).run();
-		String input;
 		boolean status = true;
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.println("Please enter a TC number (for exit enter exit or EXIT):");
-		input = scanner.next();
+		String input = scanner.nextLine();
 
 		while (status){
 			if (input.equalsIgnoreCase("exit")){
@@ -27,7 +32,7 @@ public class MyObjective2Application {
 			else {
 				Controller controller = new Controller();
 				System.out.println(controller.checkTCNumber(input));
-				input = scanner.next();
+				input = scanner.nextLine();
 			}
 		}
 		SpringApplication.exit(ctx, () -> 0);
